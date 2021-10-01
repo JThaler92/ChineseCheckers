270f8d1081a50668bab49f20688bd349b264d8fc
Johan Thaler <jthaler92@gmail.com>
2021-10-01 14:11:21 +0200

    Save and load functions added to session class

--- Classes/Session.cs
+++ Classes/Session.cs
@@ -4,11 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Media.Playback;
+using System.Xml.Serialization;
 
 namespace ChineseCheckers.Classes
 {
@@ -152,6 +154,36 @@ namespace ChineseCheckers.Classes
             var targetNode = randomMarble.Value.ElementAt(rnd.Next(0, randomMarble.Value.Count));
             Board.MarbleMove(targetNode, marble);
             Turn();
+        }
+        public static void Save<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        {
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                writer = new StreamWriter(filePath, append);
+                serializer.Serialize(writer, objectToWrite);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+        public static T Load<T>(string filePath) where T : new()
+        {
+            TextReader reader = null;
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                reader = new StreamReader(filePath);
+                return (T)serializer.Deserialize(reader);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
         }
     }
 }
